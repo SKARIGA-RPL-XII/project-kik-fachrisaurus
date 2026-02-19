ed4655b50a1be4fea9c32437add5e18076ac6802
Fachri <adhisfachrifadhilah.kotamalang@gmail.com>
2026-02-19 11:15:58 +0700

    Membuat Kerangka Sementara Halaman Profile Pengguna

--- Inlearnia/resources/views/pengajar/profile/index.blade.php
+++ Inlearnia/resources/views/pengajar/profile/index.blade.php
@@ -1,1 +1,16 @@
-ini profil saya
+@extends('layouts.app')
+
+@section('content')
+    <x-sidebar />
+
+    <div class="flex justify-between items-center mb-10">
+        <div class="flex-1">
+            <x-breadcrumb :items="[['label' => 'Profil Saya', 'url' => null]]" />
+        </div>
+        <x-calendar />
+    </div>
+
+    <div class="flex justify-between items-center mt-[40px] mb-[25px]">
+        <x-ui.title text="Profil Saya" />
+    </div>
+@endsection
