2feeaf38af4067dc39a6c220390a521bffbabad1
Fachri <adhisfachrifadhilah.kotamalang@gmail.com>
2026-02-21 17:21:11 +0700

    Update Announcement.php

--- Inlearnia/app/Models/Announcement.php
+++ Inlearnia/app/Models/Announcement.php
@@ -11,9 +11,21 @@ class Announcement extends Model
 
     protected $guarded = ['id'];
 
+    // Casting agar field JSON otomatis menjadi Array di PHP
+    protected $casts = [
+        'links' => 'array',
+        'files' => 'array',
+    ];
+
     // Relasi balik ke Kelas
     public function classRoom()
     {
         return $this->belongsTo(ClassRoom::class, 'class_id');
+    }
+    
+    // Relasi ke Pembuat Pengumuman
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
