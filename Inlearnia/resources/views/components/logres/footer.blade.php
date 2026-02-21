c1dbe16bfcc47f0448625073403794bda38ab87d
Fachri <adhisfachrifadhilah.kotamalang@gmail.com>
2026-02-21 17:15:36 +0700

    Rename Kecil

--- Inlearnia/resources/views/components/logres/footer.blade.php
+++ Inlearnia/resources/views/components/logres/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="absolute bottom-6 left-8 text-sm text-white/40">
-    © {{ date('Y') }} fachrifadhil · adhisfachrifadhilah@gmail.com
+    © {{ date('Y') }} Adhis Fachri Fadhilah · adhisfachrifadhilah@gmail.com
 </div>
